25d175407a4d9534fd7d4b501610abcd7ed59259
Zirui-Ma <ziruima@outlook.com>
2025-03-01 00:22:44 -0700

    Alpha 2.0 release of OpenCV detector, added new process for detecting the samples 0301

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PipelineColorContourDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PipelineColorContourDetection.java
@@ -12,7 +12,11 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import org.openftc.easyopencv.OpenCvCamera;
@@ -32,46 +36,175 @@ public class PipelineColorContourDetection extends LinearOpMode {
     public static class CameraProcessor extends OpenCvPipeline {
         private final Mat hsvMat = new Mat();
         private final Mat threshold = new Mat();
+        private final Mat blurred = new Mat();
+        private final Mat edged = new Mat();
+        private Mat kernel = new Mat();
+        private final Mat dilated = new Mat();
         private final Mat hierarchy = new Mat();
 
-        public static Scalar RANGE_HIGH = new Scalar(30, 255, 255);
+        public static Scalar RANGE_HIGH = new Scalar(100, 255, 255);
         public static Scalar RANGE_LOW = new Scalar(20, 100, 100);
+
+        /**
+         Range:
+            * Yellow
+                -Range_High (30, 255, 255)
+                -Range_Low (20, 100, 100)
+         */
+
+        public static int Min_Val = 200;
+        public static int Max_Val = 700;
 
         List<MatOfPoint> contours = new ArrayList<>();
 
-        public static MatOfPoint goodContour;
+        private MatOfPoint2f contour2f;
+        private MatOfPoint2f approxCurve;
+        private double epsilon;
+
+        private MatOfPoint2f points;
+
+        public static RotatedRect rotatedRect;
+
+        Point[] vertices = new Point[4];
+        MatOfPoint box;
+        List<MatOfPoint> boxPoints = new ArrayList<>();
+
+        public static RotatedRect goodRect;
+
+        private double contourArea;
+
+        /** The following is for tuning and debugging **/
+        public static boolean mask_toggle = false;
+        public static boolean blur_toggle = false;
+        public static boolean edge_toggle = false;
+        public static boolean dilate_toggle = false;
+        public static boolean approxCurve_toggle = false;
 
         @Override
         public Mat processFrame(@NonNull Mat inputFrame) {
+
             Imgproc.cvtColor(inputFrame, hsvMat, Imgproc.COLOR_RGB2HSV);
             Core.inRange(hsvMat, RANGE_LOW, RANGE_HIGH, threshold);
 
-            Imgproc.findContours(threshold, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+            Imgproc.GaussianBlur(threshold, blurred, new Size(3, 3), 0);
 
-            goodContour = findGoodContour(contours);
+            Imgproc.Canny(blurred, edged, Min_Val, Max_Val, 3, true);
 
-            if (goodContour != null) {
+            kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+            Imgproc.dilate(edged, dilated, kernel);
+
+            Imgproc.findContours(dilated, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            //goodContour = findGoodContour(contours);
+
+
+            //if (goodContour != null) {
                 /** try index of 0 later **/
-                Imgproc.drawContours(inputFrame, contours, contours.indexOf(goodContour) /** -1 **/, new Scalar(0, 255, 0), 1);
-            }
-
-            return inputFrame;
-        }
-
-        private MatOfPoint findGoodContour(@NonNull List<MatOfPoint> contours) {
-            double maxArea = 40000;
-            double minArea = 8000;
-            MatOfPoint goodContour = null;
+                //Imgproc.drawContours(inputFrame, contours, /**contours.indexOf(goodContour)**/ -1, new Scalar(0, 255, 0), 1);
+            //}
 
             for (MatOfPoint contour : contours) {
-                double area = Imgproc.contourArea(contour);
-                if (area < maxArea && area > minArea) {
-                    goodContour = contour;
+                contour2f = new MatOfPoint2f(contour.toArray());
+                approxCurve = new MatOfPoint2f();
+                epsilon = 0.02 * Imgproc.arcLength(contour2f, true);
+
+                Imgproc.approxPolyDP(contour2f, approxCurve, epsilon, true);
+
+                points = new MatOfPoint2f(approxCurve.toArray());
+                //Imgproc.drawContours(inputFrame, List.of(points), -1, new Scalar(0, 255, 0), 2);
+
+                //Rect rect = Imgproc.boundingRect(points);
+                //Imgproc.rectangle(inputFrame, rect, );
+
+                rotatedRect = Imgproc.minAreaRect(points);
+                goodRect = findGoodRect(rotatedRect);
+
+                if (goodRect != null) {
+                Imgproc.circle(inputFrame, goodRect.center, 5, new Scalar(255, 0, 0), 1);
+
+                goodRect.points(vertices);
+                box = new MatOfPoint(vertices);
+                boxPoints.add(box);
+
+                Imgproc.circle(inputFrame, vertices[0], 5, new Scalar(0, 0, 255), -1);
+                Imgproc.circle(inputFrame, vertices[1], 5, new Scalar(0, 0, 255), -1);
+                Imgproc.circle(inputFrame, vertices[2], 5, new Scalar(0, 0, 255), -1);
+                Imgproc.circle(inputFrame, vertices[3], 5, new Scalar(0, 0, 255), -1);
+
+                Imgproc.polylines(inputFrame, boxPoints, true, new Scalar(0, 255, 0), 1);
+                /**Try the chatGPT image later**/
                 }
             }
 
-            return goodContour;
+            if (contours != null && contour2f != null) {
+                releaseMemory();
+            }
+
+            if (mask_toggle) {
+                return threshold;
+            }
+            if (blur_toggle) {
+                return blurred;
+            }
+            if (edge_toggle) {
+                return edged;
+            }
+            if (dilate_toggle) {
+                return dilated;
+            }
+            if (approxCurve_toggle) {
+                return approxCurve;
+            }
+            else {
+                return inputFrame;
+            }
         }
+
+        public void releaseMemory() {
+            hsvMat.release();
+            threshold.release();
+            blurred.release();
+            kernel.release();
+            edged.release();
+            dilated.release();
+            contours.clear();
+            contour2f.release();
+            approxCurve.release();
+            points.release();
+            box.release();
+            boxPoints.clear();
+        }
+
+        //private MatOfPoint findGoodContour(@NonNull List<MatOfPoint> contours, RotatedRect rect) {
+        //    double maxArea = 40000;
+        //    double minArea = 7000;
+        //    MatOfPoint goodContour = null;
+
+        //    for (MatOfPoint contour : contours) {
+        //        double area = rect.size.area();
+        //        if (area < maxArea && area > minArea) {
+        //            goodContour = contour;
+        //        }
+        //    }
+
+        //    return goodContour;
+        //}
+
+        private RotatedRect findGoodRect (@NonNull RotatedRect rect) {
+            double maxArea = 40000;
+            double minArea = 8500;
+
+            //Integer[] width = {125, 90};
+            //Integer[] height = {295, 73};
+            RotatedRect goodRect = null;
+
+            if (rect.size.area() < maxArea && rect.size.area() > minArea) {
+                goodRect = rect;
+            }
+
+            return goodRect;
+        }
+
     }
 
 
@@ -92,8 +225,13 @@ public class PipelineColorContourDetection extends LinearOpMode {
 
         while (opModeIsActive()) {
             FtcDashboard.getInstance().startCameraStream(camera, 30);
+            if (pipeline.rotatedRect != null) {
+                telemetry.addData("Sample angle", pipeline.rotatedRect.angle);
+                telemetry.addData("Sample center", pipeline.rotatedRect.center);
+                telemetry.addData("Sample size", pipeline.rotatedRect.size);
+                telemetry.update();
+            }
             //telemetry.addData("Contour index", pipeline.contours.indexOf(pipeline.goodContour));
-            telemetry.update();
         }
         camera.stopStreaming();
     }
