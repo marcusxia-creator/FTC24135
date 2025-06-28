4a65cd011d6aed9a9ba49e43d1cf755b81ae08ba
Sylvan-Z <Sylvanz20@gmail.com>
2025-06-28 12:53:09 -0600

    Created vision FSM

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/FiniteStateMachineVision.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.TeleOps;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+public class FiniteStateMachineVision {
+
+    public enum VISIONSTATE{
+        IDLE,
+
+        VISION_COARSE_DETECT,
+        VISION_COARSE_EXTEND,
+
+        VISION_FINE
+    }
+
+    public VISIONSTATE visionState;
+
+    private final GamepadEx gamepad_1;
+    private final GamepadEx gamepad_2;
+    private final RobotHardware robot;
+    private final FiniteStateMachineIntake intakeArmDrive;
+
+    public FiniteStateMachineVision(RobotHardware robot, GamepadEx gamepad_1, GamepadEx gamepad_2, FiniteStateMachineIntake intakeArmDrive) {
+        this.gamepad_1 = gamepad_1;
+        this.gamepad_2 = gamepad_2;
+        this.robot = robot;
+        this.intakeArmDrive = intakeArmDrive;
+        this.visionState = VISIONSTATE.IDLE;
+    }
+
+    public void Init(){
+
+    }
+
+    public void VisionLoop(){
+        switch(visionState){
+            case IDLE:
+
+                break;
+
+            case VISION_COARSE_DETECT:
+
+                break;
+
+            case VISION_COARSE_EXTEND:
+
+                break;
+
+            case VISION_FINE:
+
+                break;
+        }
+    }
+}
