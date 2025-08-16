9e0933c02523f5138e15c2aae31a2d18d405b2a7
Sylvan-Z <Sylvanz20@gmail.com>
2025-08-15 22:18:58 -0600

    Made Sample.java look better, and added comments to Sample.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/coarsevisionproc/Sample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/coarsevisionproc/Sample.java
@@ -8,11 +8,14 @@ public class Sample {
 
     public Point ViscenterPoint;
     public Point relPos;
-    public ColorBlobLocatorProcessor.Blob blob;
     public double ODistance;
     public double Orientation;
 
+    public ColorBlobLocatorProcessor.Blob blob;
+
     public Sample(ColorBlobLocatorProcessor.Blob blob, Pose3D relCam, CamFieldProfile CamProfile){
+
+        //Finding relPos using algorithm
         this.ViscenterPoint=CamProfile.PixelToAngle(blob.getBoxFit().center);
 
         double a=Math.toRadians(relCam.getOrientation().getPitch());
@@ -25,11 +28,14 @@ public class Sample {
                 h*(-(Math.tan(x)*Math.cos(a)/Math.tan(a-y))-(Math.tan(x)*Math.sin(a))),
                 h*(1/Math.tan(a-y))+relCam.getPosition().y);
 
+        //Distance (For FindBestSample)
         this.ODistance=Math.sqrt(Math.pow(this.relPos.x,2)+Math.pow(this.relPos.y,2));
 
-        Point[] points = blob.getContourPoints();
+        //Find Orientation
+        Point[] points = blob.getContourPoints(); //Theoretically shouldn't work, check size of array with telemetry later
         double deltax;
         double deltay;
+        //Use longest edge
         if(Math.sqrt(Math.pow(points[0].x-points[1].x,2)+Math.pow(points[0].y-points[1].y,2))>
                 Math.sqrt(Math.pow(points[1].x-points[2].x,2)+Math.pow(points[1].y-points[2].y,2))
         ){deltax = points[0].x-points[1].x; deltay = points[0].y-points[1].y;}
@@ -38,7 +44,7 @@ public class Sample {
         if(deltax!=0){
             this.Orientation=Math.atan(deltay/deltax)-(Math.PI/2);
         } else {
-            this.Orientation=0;
+            this.Orientation=0; //Avoid atan(90°)
         }
 
         this.blob = blob;
