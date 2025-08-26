31c737de1f05c7bb3fc174eb2703e4d4fb24341b
Sylvan-Z <Sylvanz20@gmail.com>
2025-08-25 22:46:54 -0600

    Added rotatePose helper function, streamlined robot centric, and created loop function to run control mode FSM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IceWaddler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IceWaddler.java
@@ -12,16 +12,32 @@ import org.firstinspires.ftc.teamcode.TeleOps.RobotHardware;
 
 public class IceWaddler {
     final RobotHardware robot;
-    public GoBildaPinpointDriver odo;
 
+    //Hardware Definition
     public DcMotorEx frontLeftMotor;
     public DcMotorEx backLeftMotor;
     public DcMotorEx frontRightMotor;
     public DcMotorEx backRightMotor;
 
+    public GoBildaPinpointDriver odo;
+
+    //global odo data
+    public Pose2D currentPos;
+    public Pose2D currentVel;
+
+    //Power Pose2D object
+    public Pose2D targetPower;
+
+    //Velocity Pose2D object
+    public Pose2D targetVel;
+
     //Velocity to Power PID Controllers
     public PIDController vController;
     public PIDController vRotController;
+
+    //Position Pose2D object
+    public Pose2D startingPos;
+    public Pose2D targetPos;
 
     public enum CONTROLMODE {
         POWER,
@@ -37,18 +53,9 @@ public class IceWaddler {
         this.robot=robot;
     }
 
-    public void InitOdo(Pose2D initPose){
-        odo=robot.odo;
 
-        odo.setOffsets(IceWaddlerConfig.odoXOffset, IceWaddlerConfig.odoYOffset); //these are tuned for 3110-0002-0001 Product Insight #1
-        odo.setEncoderResolution(IceWaddlerConfig.odoEncoderResolution);
-        odo.setEncoderDirections(IceWaddlerConfig.xEncoderDirection, IceWaddlerConfig.yEncoderDirection);
-        //Set to start counting at initPose parameter
-        odo.resetPosAndIMU();
-        odo.setPosition(initPose);
-    }
 
-    public void InitDrive(CONTROLMODE InitMode, boolean fieldCentric){
+    public void InitDrive(CONTROLMODE InitMode, Pose2D initPose, boolean fieldCentric){
         controlMode = InitMode;
         this.fieldCentric = fieldCentric;
 
@@ -59,22 +66,50 @@ public class IceWaddler {
 
         vController = IceWaddlerConfig.vController;
         vRotController = IceWaddlerConfig.vRotController;
+
+        InitOdo(initPose);
     }
 
-    private void writePower(Pose2D targetPower, Pose2D currentPos, Pose2D currentVel){
-        if(fieldCentric) {
-            double currentHeading = currentPos.getHeading(AngleUnit.RADIANS);
+    public void InitOdo(Pose2D initPose){
+        odo=robot.odo;
 
-            targetPower = new Pose2D(DistanceUnit.METER,
-                    targetPower.getX(DistanceUnit.METER)*Math.cos(currentHeading)-targetPower.getY(DistanceUnit.METER)*Math.sin(currentHeading),
-                    targetPower.getX(DistanceUnit.METER)*Math.sin(currentHeading)+targetPower.getY(DistanceUnit.METER)*Math.cos(currentHeading),
-                    AngleUnit.RADIANS, targetPower.getHeading(AngleUnit.RADIANS)
-            );
-        }
+        odo.setOffsets(IceWaddlerConfig.odoXOffset, IceWaddlerConfig.odoYOffset); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setEncoderResolution(IceWaddlerConfig.odoEncoderResolution);
+        odo.setEncoderDirections(IceWaddlerConfig.xEncoderDirection, IceWaddlerConfig.yEncoderDirection);
+        //Set to start counting at initPose parameter
+        odo.resetPosAndIMU();
+        odo.setPosition(initPose);
 
-        double x = targetPower.getX(DistanceUnit.METER);
-        double y = targetPower.getY(DistanceUnit.METER);
-        double rot = targetPower.getHeading(AngleUnit.DEGREES);
+        updateOdo();
+    }
+
+    public void updateOdo(){
+        odo.update();
+
+        currentPos = odo.getPosition();
+        currentVel = odo.getVelocity();
+    }
+
+    public void setFieldCentric(boolean v){
+        fieldCentric = v;
+    }
+
+    public void toggleFieldCentric(){
+        fieldCentric = !fieldCentric;
+    }
+
+    public void runByPower(Pose2D targetPower){
+        controlMode = CONTROLMODE.POWER;
+        this.targetPower = targetPower;
+    }
+
+    private void writePower(){
+
+        Pose2D robotCentricPower = rotatePose(targetPower, AngleUnit.RADIANS, -currentPos.getHeading(AngleUnit.RADIANS));
+
+        double x = robotCentricPower.getX(DistanceUnit.METER);
+        double y = robotCentricPower.getY(DistanceUnit.METER);
+        double rot = robotCentricPower.getHeading(AngleUnit.RADIANS);
 
         frontLeftMotor.setPower(-x-y+rot);
         backLeftMotor.setPower(-x+y+rot);
@@ -82,17 +117,61 @@ public class IceWaddler {
         backRightMotor.setPower(x+y+rot);
     }
 
-    private void writeVel(Pose2D targetVel, Pose2D currentPos, Pose2D currentVel) {
+    public void runByVel(Pose2D targetVel){
+        controlMode = CONTROLMODE.VELOCITY;
+        this.targetVel = targetVel;
+    }
 
-        Pose2D targetPower = new Pose2D(
+    private void writeVel() {
+
+        targetPower = new Pose2D(
                 DistanceUnit.METER,
                 vController.calculate(currentVel.getX(DistanceUnit.METER), targetVel.getX(DistanceUnit.METER)),
                 vController.calculate(currentVel.getY(DistanceUnit.METER), targetVel.getY(DistanceUnit.METER)),
-
                 AngleUnit.RADIANS,
                 vRotController.calculate(currentVel.getHeading(AngleUnit.RADIANS), targetVel.getHeading(AngleUnit.RADIANS)));
 
-        writePower(targetPower, currentPos, currentVel);
+        writePower();
     }
 
+    public void loop(){
+
+        updateOdo();
+
+        switch (controlMode){
+            case POWER:
+                //Apply robot centric with fresh odo data
+                if(!fieldCentric){
+                    targetPower=rotatePose(targetPower, AngleUnit.RADIANS, currentPos.getHeading(AngleUnit.RADIANS));
+                }
+                writePower();
+                break;
+
+            case VELOCITY:
+                //Apply robot centric with fresh odo data
+                if(!fieldCentric){
+                    targetVel=rotatePose(targetVel, AngleUnit.RADIANS, currentPos.getHeading(AngleUnit.RADIANS));
+                }
+                writeVel();
+                break;
+
+            case PTP:
+                //Not yet implemented
+                break;
+        }
+    }
+
+    //Helper Function
+    private Pose2D rotatePose(Pose2D pose, AngleUnit angleUnit, double angle){
+
+        angle=AngleUnit.RADIANS.fromUnit(angleUnit,angle);
+
+        return new Pose2D(
+                DistanceUnit.METER,
+                pose.getX(DistanceUnit.METER)*Math.cos(angle)-pose.getY(DistanceUnit.METER)*Math.sin(angle),
+                pose.getX(DistanceUnit.METER)*Math.sin(angle)+pose.getY(DistanceUnit.METER)*Math.cos(angle),
+                AngleUnit.RADIANS,
+                pose.getHeading(AngleUnit.RADIANS)
+        );
+    }
 }
